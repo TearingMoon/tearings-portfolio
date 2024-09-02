34cba2402397c3e6a3f36f803398b518e78c94b8
David Torrubia <davidtorrubiasantos@gmail.com>
2024-09-02 04:14:22 +0200

    refactor: Update ProyectsListComponent.vue to add category filter buttons and project items
    - Added category filter buttons for Defense, Web, Systems, AI, and Hackatons
    - Updated project items to display title, description, technologies, and link
    - Created ProyectsListItemComponent.vue to handle individual project items

--- src/components/views/home/ProyectsListComponent.vue
+++ src/components/views/home/ProyectsListComponent.vue
@@ -1,25 +1,114 @@
 <template>
   <!-- Search Filter -->
-  <div class="flex flex-row justify-center items-center text-xl flex-wrap">
-    <button class="text-white/50 hover:text-white transition duration-150">Defense</button>
+  <div class="flex flex-row justify-center items-center text-xl flex-wrap mb-10">
+    <button
+      class="text-white/50 hover:text-white transition duration-150"
+      @click="filterCategory = 'Defense'"
+    >
+      Defense
+    </button>
     <span class="text-white/50 mx-4"> // </span>
-    <button class="text-white/50 hover:text-white transition duration-150">Defense</button>
+    <button
+      class="text-white/50 hover:text-white transition duration-150"
+      @click="filterCategory = 'Web'"
+    >
+      Web
+    </button>
     <span class="text-white/50 mx-4"> // </span>
-    <button class="text-white/50 hover:text-white transition duration-150">Defense</button>
+    <button
+      class="text-white/50 hover:text-white transition duration-150"
+      @click="filterCategory = 'Systems'"
+    >
+      Systems
+    </button>
     <span class="text-white/50 mx-4"> // </span>
-    <button class="text-white/50 hover:text-white transition duration-150">Defense</button>
+    <button
+      class="text-white/50 hover:text-white transition duration-150"
+      @click="filterCategory = 'AI'"
+    >
+      AI
+    </button>
     <span class="text-white/50 mx-4"> // </span>
-    <button class="text-white/50 hover:text-white transition duration-150">Defense</button>
+    <button
+      class="text-white/50 hover:text-white transition duration-150"
+      @click="filterCategory = 'Hackatons'"
+    >
+      Hackatons
+    </button>
   </div>
-  <div class="flex"></div>
+  <div class="flex flex-row flex-wrap flex-1 w-full justify-center gap-10 min-h-36">
+    <ProyectsListItemComponent
+      :categories="['Defense', 'Systems', 'Personal']"
+      :filter-category="filterCategory"
+      link="https://github.com/TearingMoon/StormWorks-Microcontrollers/tree/main/Refined%20Radar%20Controller"
+    >
+      <template #content> </template>
+      <template #title> Radar Display</template>
+      <template #description>
+        Personal project to display radar data in a more visual way. It was developed by personal
+        research and designed for defense uses.
+      </template>
+      <template #technologies> Lua </template>
+    </ProyectsListItemComponent>
+    <ProyectsListItemComponent
+      :categories="['Web', 'Personal', 'Professional']"
+      :filter-category="filterCategory"
+      link=""
+    >
+      <template #content> </template>
+      <template #title> Slou Web Page</template>
+      <template #description>
+        A web page developed for a family member's company. It was developed using Vue.js and
+        TailwindCSS.
+      </template>
+      <template #technologies> Vue.js, Tailwind.</template>
+    </ProyectsListItemComponent>
+    <ProyectsListItemComponent
+      :categories="['Web', 'Personal', 'Hackatons', 'AI']"
+      :filter-category="filterCategory"
+      link=""
+    >
+      <template #content> </template>
+      <template #title> NASA Space Apps Zaragoza AI (3rd Place)</template>
+      <template #description>
+        A Nlp AI developed for the NASA Space Apps hackaton. It was developed using React.js and
+        Python with OpenSearch. It was designed to filter and search for information in a more human
+        way inside the NASA documentation.
+      </template>
+      <template #technologies> React.js, Python, OpenSearch.</template>
+    </ProyectsListItemComponent>
+    <ProyectsListItemComponent
+      :categories="['Personal', 'Hackatons']"
+      :filter-category="filterCategory"
+      link=""
+    >
+      <template #content> </template>
+      <template #title> CodeLabs Zaragoza Bruteforce (1st Place)</template>
+      <template #description>
+        A bruteforce Computer Interface developed for the CodeLabs Zaragoza hackaton. It was
+        developed in a team of 4 people and it was designed to find the best path between certain
+        cities in spain with the best setting.
+      </template>
+      <template #technologies> C#, .NET </template>
+    </ProyectsListItemComponent>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import ProyectsListItemComponent from './ProyectsListItemComponent.vue'
 
 export default defineComponent({
   setup() {
     return {}
+  },
+  data() {
+    return {
+      filterCategory: 'Defense'
+    }
+  },
+  components: {
+    ProyectsListItemComponent
   }
 })
 </script>
---
+++ src/components/views/home/ProyectsListItemComponent.vue
@@ -0,0 +1,64 @@
+<template>
+  <transition
+    enter-active-class="animate__animated animate__fadeIn"
+    leave-active-class="animate__animated animate__fadeOut"
+  >
+    <div
+      v-if="categories.includes(filterCategory)"
+      @click="redirectTo"
+      class="p-10 bg-gray-800 shadow-lg w-2/5 interactable"
+    >
+      <slot name="content"> </slot>
+      <h2 class="font-bold text-2xl my-5 text-center"><slot name="title"></slot></h2>
+      <p>
+        <strong>Description:</strong>
+        <slot name="description"></slot>
+      </p>
+      <p>
+        <strong>Technologies:</strong>
+        <slot name="technologies"></slot>
+      </p>
+      <div class="flex flex-row gap-4 flex-wrap mt-5">
+        <div class="border-2 p-2" v-for="(category, index) in categories" :key="index">
+          {{ category }}
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+  data() {
+    return {}
+  },
+  props: {
+    categories: {
+      type: Array,
+      required: true
+    },
+    filterCategory: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: false
+    }
+  },
+  methods: {
+    redirectTo() {
+      if (this.link) {
+        window.open(this.link, '_blank')
+      }
+    }
+  }
+})
+</script>
+
+<style scoped></style>
