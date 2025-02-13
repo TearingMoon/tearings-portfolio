3d863481dce0217559a18c535a9a3c90950492d8
David Torrubia <davidtorrubiasantos@gmail.com>
2025-02-13 01:07:53 +0100

    Refactor TypingEffect component to use Composition API and improve typing logic

--- src/components/utils/TypingEffect.vue
+++ src/components/utils/TypingEffect.vue
@@ -5,48 +5,48 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
 
-export default defineComponent({
-  name: 'TypingEffect',
-  setup() {
-    return {}
-  },
-  data() {
-    return {
-      displayedText: '',
-      currentIndex: 0,
-      showCursor: true
-    }
-  },
-  mounted() {
-    this.startTyping()
-  },
+var displayedText = ref('')
+var currentIndex = ref(0)
 
-  props: {
-    text: {
-      type: String,
-      required: true
-    },
-    typingSpeed: {
-      type: Number,
-      default: 100
-    }
+const emit = defineEmits(['doneTyping'])
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
   },
-
-  methods: {
-    startTyping() {
-      const typingInterval = setInterval(() => {
-        if (this.currentIndex < this.text.length) {
-          this.displayedText += this.text[this.currentIndex]
-          this.currentIndex++
-        } else {
-          clearInterval(typingInterval)
-        }
-      }, this.typingSpeed)
-    }
+  typingSpeed: {
+    type: Number,
+    default: 100
+  },
+  autoStart: {
+    type: Boolean,
+    default: true
   }
+})
+
+function startTyping() {
+  const typingInterval = setInterval(() => {
+    if (currentIndex.value < props.text.length) {
+      displayedText.value += props.text[currentIndex.value]
+      currentIndex.value++
+    } else {
+      emit('doneTyping')
+      clearInterval(typingInterval)
+    }
+  }, props.typingSpeed)
+}
+
+onMounted(() => {
+  if (props.autoStart) {
+    startTyping()
+  }
+})
+
+defineExpose({
+  startTyping
 })
 </script>
 
