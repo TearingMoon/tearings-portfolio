0b33f04bdbb26b065f44d345e390b84f16710166
David Torrubia <davidtorrubiasantos@gmail.com>
2025-02-16 01:26:06 +0100

    Replace HomeView with WelcomeComponent and refactor typing logic

---
+++ src/components/views/home/WelcomeComponent.vue
@@ -0,0 +1,119 @@
+<template>
+  <SectionComponent class="w-full h-screen flex flex-col justify-center items-center">
+    <transition
+      enter-active-class="animate__animated animate__fadeIn"
+      leave-active-class="animate__animated animate__fadeOut"
+    >
+      <div
+        v-if="firstPhase"
+        class="absolute w-full h-full flex flex-col justify-center items-center"
+      >
+        <svg
+          viewBox="0 0 100 100"
+          xmlns="http://www.w3.org/2000/svg"
+          class="absolute w-1/2 h-1/2 z-10"
+        >
+          <circle
+            ref="LoadingCircle"
+            cx="50"
+            cy="50"
+            r="40"
+            transform="rotate(-90 50 50)"
+            stroke="white"
+            stroke-width="6"
+            fill="none"
+            stroke-dasharray="251.2"
+            stroke-dashoffset="251.2"
+          >
+            <animate
+              attributeName="stroke-dashoffset"
+              from="251.2"
+              to="0"
+              dur="2s"
+              fill="freeze"
+              ref="loadingCircleAnimation"
+            />
+          </circle>
+        </svg>
+        <h1 class="text-4xl font-extrabold text-white absolute">Loading</h1>
+      </div>
+    </transition>
+
+    <transition
+      enter-active-class="animate__animated animate__fadeIn"
+      leave-active-class="animate__animated animate__fadeOut"
+    >
+      <div class="absolute w-full h-screen z-0 text-neutral-500" v-if="secondPhase">
+        <div class="w-full h-full">
+          <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <pattern id="grid" patternUnits="userSpaceOnUse" width="80" height="80">
+                <path d="M 80 0 L 0 0 0 80" fill="none" stroke="currentColor" stroke-width="1" />
+              </pattern>
+            </defs>
+            <rect width="100%" height="100%" fill="url(#grid)" />
+          </svg>
+        </div>
+      </div>
+    </transition>
+    <TypingEffect
+      ref="nameTypingEffect"
+      text="David Torrubia"
+      class="text-8xl font-extrabold text-white z-10 my-4"
+      @doneTyping="finishTyping"
+      :auto-start="false"
+      v-if="secondPhase"
+    ></TypingEffect>
+    <Transition
+      enter-active-class="animate__animated animate__flipInX"
+      leave-active-class="animate__animated animate__flipOutX"
+    >
+      <h1 class="text-white text-2xl z-10" v-if="thirdPhase">
+        Software Engineer | Full Stack Developer
+      </h1>
+    </Transition>
+  </SectionComponent>
+</template>
+
+<script setup lang="ts">
+import TypingEffect from '@/components/utils/TypingEffect.vue'
+import SectionComponent from '@/components/utils/SectionComponent.vue'
+import { onMounted, ref, useTemplateRef } from 'vue'
+
+const nameTypingEffect = useTemplateRef('nameTypingEffect')
+const LoadingCircle = useTemplateRef('LoadingCircle')
+const animate = useTemplateRef('loadingCircleAnimation')
+
+const firstPhase = ref(true)
+const secondPhase = ref(false)
+const thirdPhase = ref(false)
+
+function startTyping() {
+  if (animate.value) {
+    animate.value?.removeEventListener('endEvent', startTyping)
+  }
+
+  firstPhase.value = false
+  secondPhase.value = true
+
+  setTimeout(() => {
+    if (nameTypingEffect.value) {
+      nameTypingEffect.value.startTyping()
+    }
+  }, 1000)
+}
+
+function finishTyping() {
+  thirdPhase.value = true
+}
+
+onMounted(() => {
+  if (LoadingCircle.value) {
+    if (animate.value) {
+      animate.value.addEventListener('endEvent', startTyping)
+    }
+  }
+})
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,95 +1,11 @@
 <template>
   <main>
-    <SectionComponent class="w-full h-screen flex flex-col justify-center items-center">
-      <transition
-        enter-active-class="animate__animated animate__fadeIn"
-        leave-active-class="animate__animated animate__fadeOut"
-      >
-        <svg
-          viewBox="0 0 100 100"
-          xmlns="http://www.w3.org/2000/svg"
-          class="absolute w-1/2 h-1/2 z-10"
-          v-if="isCircleVisible"
-        >
-          <circle
-            ref="circle"
-            cx="50"
-            cy="50"
-            r="40"
-            transform="rotate(-90 50 50)"
-            stroke="white"
-            stroke-width="3"
-            fill="none"
-            stroke-dasharray="251.2"
-            stroke-dashoffset="251.2"
-          >
-            <animate attributeName="stroke-dashoffset" from="251.2" to="0" dur="2s" fill="freeze" />
-          </circle>
-        </svg>
-      </transition>
-      <transition
-        enter-active-class="animate__animated animate__fadeIn"
-        leave-active-class="animate__animated animate__fadeOut"
-      >
-        <div class="absolute w-full h-screen z-0 text-neutral-500" v-if="isGridVisible">
-          <div class="w-full h-full">
-            <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
-              <defs>
-                <pattern id="grid" width="50" height="50" patternUnits="userSpaceOnUse">
-                  <path d="M 80 0 L 0 0 0 80" fill="none" stroke="currentColor" stroke-width="1" />
-                </pattern>
-              </defs>
-              <rect width="100%" height="100%" fill="url(#grid)" />
-            </svg>
-          </div>
-        </div>
-      </transition>
-      <TypingEffect
-        ref="nameTyping"
-        text="David Torrubia"
-        class="text-8xl font-extrabold text-white z-10"
-        @doneTyping="showEnd"
-        :auto-start="false"
-        v-if="isTypingVisible"
-      ></TypingEffect>
-    </SectionComponent>
+    <WelcomeComponent />
   </main>
 </template>
 
 <script lang="ts" setup>
-import TypingEffect from '@/components/utils/TypingEffect.vue'
-import SectionComponent from '@/components/utils/SectionComponent.vue'
-import { onMounted, ref, useTemplateRef } from 'vue'
-
-const startTyping = useTemplateRef('nameTyping')
-const circle = useTemplateRef('circle')
-
-const isCircleVisible = ref(true)
-const isTypingVisible = ref(false)
-const isGridVisible = ref(false)
-
-function start() {
-  isCircleVisible.value = false
-  isTypingVisible.value = true
-  isGridVisible.value = true
-
-  setTimeout(() => {
-    if (startTyping.value) {
-      startTyping.value.startTyping()
-    }
-  }, 1000)
-}
-
-function showEnd() {}
-
-onMounted(() => {
-  if (circle.value) {
-    const animate = circle.value.querySelector('animate')
-    if (animate) {
-      animate.addEventListener('endEvent', start)
-    }
-  }
-})
+import WelcomeComponent from '@/components/views/home/WelcomeComponent.vue'
 </script>
 
 <style scoped></style>
