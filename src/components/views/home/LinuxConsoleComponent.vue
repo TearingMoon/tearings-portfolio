9c9bda737553eb95314e4217c6cebbc8bfb06335
David Torrubia <davidtorrubiasantos@gmail.com>
2025-02-25 19:57:12 +0100

    Enhance LinuxConsoleComponent with command execution functionality and improve console layout; add command handling for help, clear, and about commands

--- src/components/views/home/LinuxConsoleComponent.vue
+++ src/components/views/home/LinuxConsoleComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full border-2 border-neutral-800 flex flex-col rounded-2xl box-content shadow-md h-full shadow-black overflow-hidden bg-black"
+    class="w-full relative border-2 border-neutral-800 flex flex-col rounded-2xl box-content shadow-md h-full shadow-black overflow-hidden bg-black"
     @click="ConsoleClicked"
   >
     <!-- Hidden input -->
@@ -10,7 +10,6 @@
       v-model="hiddenInputText"
       @keydown="handleKeydown"
       ref="hiddenInput"
-      autofocus
     />
     <!-- Header Container -->
     <div class="w-full h-10 bg-neutral-700 shadow flex flex-row items-center px-5">
@@ -23,17 +22,76 @@
       </svg>
     </div>
     <!-- Console Container -->
-    <div class="h-full p-5 overflow-y-auto flex flex-col customScrolbar"></div>
+    <div class="w-full h-full flex overflow-y-auto flex-col customScrolbar">
+      <div class="px-5 pt-5 text-left" ref="consoleContainer"></div>
+      <div class="px-5 pb-5">
+        <span class="text-green-400 w-full"> guest@localhost:~$ </span> {{ hiddenInputText }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-const currentProgram = ref('AboutMe')
+import { ref, onMounted, useTemplateRef } from 'vue'
 
+enum commandType {
+  EXECUTABLE = 'executable',
+  COMMAND = 'command'
+}
+
+class command {
+  name: string
+  description: string
+  type: commandType
+  execute: () => void
+  constructor(
+    name: string,
+    description: string,
+    execute: () => void,
+    type: commandType = commandType.COMMAND
+  ) {
+    this.name = name
+    this.description = description
+    this.execute = execute
+    this.type = type
+  }
+}
+
+const currentProgram = ref('')
+
+const hiddenInput = useTemplateRef('hiddenInput')
+const consoleContainer = useTemplateRef('consoleContainer')
 const hiddenInputText = ref('')
 
-onMounted(() => {})
+const commands = [
+  new command(
+    'startingMsg.sh',
+    'Shows starting message',
+    () => startingMessageCommand(),
+    commandType.EXECUTABLE
+  ),
+  new command('about.sh', 'Shows about me', () => aboutCommand(), commandType.EXECUTABLE),
+  new command(
+    'contact.sh',
+    'Shows contact me',
+    () => console.log('contact'),
+    commandType.EXECUTABLE
+  ),
+  new command(
+    'projects.sh',
+    'Shows projects',
+    () => console.log('projects'),
+    commandType.EXECUTABLE
+  ),
+  new command('help', 'Shows available commands', () => helpCommand()),
+  new command('clear', 'Clears console', () => clearCommand()),
+  new command('ls', 'List directory contents and executables', () => lsCommand())
+]
+
+onMounted(() => {
+  hiddenInputText.value = 'startingMsg.sh'
+  executeCommand()
+})
 
 const handleKeydown = (event: KeyboardEvent) => {
   if (event.key === 'Enter') {
@@ -42,14 +100,65 @@ const handleKeydown = (event: KeyboardEvent) => {
 }
 
 function executeCommand() {
-  console.log('Command executed')
-  console.log(hiddenInputText.value)
+  if (consoleContainer.value) {
+    consoleContainer.value.innerHTML += `<span class="text-green-400 w-full"> guest@localhost:~$ </span> ${hiddenInputText.value}  <br>`
+
+    for (const command of commands) {
+      if (hiddenInputText.value === command.name) {
+        currentProgram.value = command.name
+        command.execute()
+        break
+      }
+    }
+  }
+  hiddenInputText.value = ''
 }
 
 function ConsoleClicked() {
-  console.log('Console clicked')
-  const hiddenInput = document.getElementById('hiddenInput')
-  hiddenInput?.focus()
+  if (hiddenInput.value) {
+    hiddenInput?.value.focus({ preventScroll: true })
+  }
+}
+
+function helpCommand() {
+  if (consoleContainer.value) {
+    for (const command of commands) {
+      if (command.type !== commandType.EXECUTABLE)
+        consoleContainer.value.innerHTML += `${command.name} <span class="text-neutral-500">- ${command.description} </span> <br>`
+    }
+  }
+}
+
+function lsCommand() {
+  if (consoleContainer.value) {
+    consoleContainer.value.innerHTML += '<span class="font-bold"> /usr/local/bin/ <span> <br>'
+    for (const command of commands) {
+      if (command.type === commandType.EXECUTABLE)
+        consoleContainer.value.innerHTML += `${command.name} <span class="text-neutral-500">- ${command.description} </span> <br>`
+    }
+  }
+}
+
+function clearCommand() {
+  if (consoleContainer.value) {
+    consoleContainer.value.innerHTML = ''
+  }
+}
+
+function startingMessageCommand() {
+  if (consoleContainer.value) {
+    consoleContainer.value.innerHTML += `<span class="text-xl font-bold">Welcome to my linux console! ;) </span><br>`
+    consoleContainer.value.innerHTML += `Try to type '<span class="text-yellow-500">help</span>' to see avaliable commands <br>`
+    consoleContainer.value.innerHTML += `Or type '<span class="text-purple-500">about.sh</span>' to open my aboutMe <br>`
+  }
+}
+
+function aboutCommand() {
+  if (consoleContainer.value) {
+    consoleContainer.value.innerHTML += `<span class="text-xl font-bold">About me </span><br>`
+    consoleContainer.value.innerHTML += `I'm a software engineer, Videogame developer and full stack developer passionate about technology and I love to learn new things and share my knowledge with others. I'm always looking for new challenges and opportunities to grow as a professional in this awesome world of technology. <br>`
+    consoleContainer.value.innerHTML += ` <br>`
+  }
 }
 </script>
 
