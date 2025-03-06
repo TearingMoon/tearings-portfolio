fadc4b59b5b4eed81c4b02baa3f0153843673ca8
David Torrubia <davidtorrubiasantos@gmail.com>
2025-03-06 10:11:52 +0100

    Remove unused logo and cursor component; simplify WelcomeComponent and HomeView layout for improved performance and readability

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,6 @@
 <template>
   <main>
-    <Transition
-      enter-active-class="animate__animated animate__fadeInDown"
-      leave-active-class="animate__animated animate__fadeOutDown"
-    >
-      <div
-        class="w-full h-20 fixed text-white flex flex-row gap-3 items-center justify-center z-20"
-        v-if="showHeader"
-      >
-        <div
-          class="flex flex-row gap-3 items-center justify-center bg-neutral-900 px-3 border-2 rounded-2xl border-neutral-700 shadow-md shadow-black"
-        >
-          <a href="#about" class="hover:underline">About</a>
-          <p>//</p>
-          <a href="#projects" class="hover:underline">Projects</a>
-          <p>//</p>
-          <a href="#contact" class="hover:underline">Contact</a>
-        </div>
-      </div>
-    </Transition>
-    <WelcomeComponent @animation-end="showHeader = true" />
+    <WelcomeComponent />
     <AboutComponent />
     <ProjectsComponent />
     <ContactComponent />
@@ -31,9 +12,6 @@ import WelcomeComponent from '@/components/views/home/WelcomeComponent.vue'
 import AboutComponent from '@/components/views/home/AboutComponent.vue'
 import ProjectsComponent from '@/components/views/home/ProjectsComponent.vue'
 import ContactComponent from '@/components/views/home/ContactComponent.vue'
-import { ref } from 'vue'
-
-const showHeader = ref(false)
 </script>
 
 <style scoped></style>
