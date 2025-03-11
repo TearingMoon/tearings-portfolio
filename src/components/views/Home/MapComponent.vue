3e5e1e6f4c476040f0619ba39a75dc9e5f813c80
David Torrubia <davidtorrubiasantos@gmail.com>
2025-03-11 12:03:09 +0100

    Refactor MapComponent to use SVG for improved rendering; implement dynamic viewBox adjustment on resize for better responsiveness

--- src/components/views/Home/MapComponent.vue
+++ src/components/views/Home/MapComponent.vue
@@ -1,7 +1,28 @@
 <template>
-  <main class="w-full h-full text-green-500"></main>
+  <svg class="w-full h-full text-green-500" :view-box="ViewBox" ref="MainSvg"></svg>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useTemplateRef, ref, onMounted, onBeforeUnmount } from 'vue'
+
+const MainSvg = useTemplateRef('MainSvg')
+const ViewBox = ref('0 0 100 100')
+
+onMounted(() => {
+  updateViewBox()
+  window.addEventListener('resize', updateViewBox)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateViewBox)
+})
+
+const updateViewBox = () => {
+  if (MainSvg.value) {
+    const { width, height } = MainSvg.value.getBoundingClientRect()
+    ViewBox.value = `0 0 ${width} ${height}`
+  }
+}
+</script>
 
 <style scoped></style>
