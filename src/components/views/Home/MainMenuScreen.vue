1d539df61f9d8ac723719c0318a917f5230acd02
David Torrubia <davidtorrubiasantos@gmail.com>
2025-03-14 03:38:10 +0100

    Refactor MainMenuScreen layout for improved responsiveness; adjust styling for better user experience on larger screens

--- src/components/views/Home/MainMenuScreen.vue
+++ src/components/views/Home/MainMenuScreen.vue
@@ -1,35 +1,35 @@
 <template>
-  <main class="flex flex-col md:flex-row items-center justify-center h-screen w-screen">
-    <p class="absolute bottom-0 text-green-700 text-xs md:text-lg">
+  <main class="flex flex-col lg:flex-row items-center justify-center h-screen w-screen">
+    <p class="absolute bottom-0 text-green-700 text-xs lg:text-lg">
       Connection from: {{ ip }} - Secure connection established
     </p>
-    <p class="absolute top-0 text-green-700 text-xs md:text-lg">C2 - CRT v1.2.3</p>
+    <p class="absolute top-0 text-green-700 text-xs lg:text-lg">C2 - CRT v1.2.3</p>
     <div
-      class="items-center md:gap-y-10 w-full md:w-1/4 md:h-full md:pr-0 px-10 pt-10 md:py-10 justify-center md:justify-normal grid grid-cols-2 md:grid-cols-1 gap-4"
+      class="items-center lg:gap-y-10 w-full lg:w-1/4 lg:h-full lg:pr-0 px-5 lg:px-10 pt-10 lg:py-10 justify-center lg:justify-normal grid grid-cols-2 lg:grid-cols-1 gap-4"
     >
       <button
-        class="text-green-500 md:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn animate__delay-2s flex flex-col items-center justify-center"
+        class="text-green-500 lg:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn animate__delay-2s flex flex-col items-center justify-center"
         @click="MapComponentRef?.goToPoint('Uplink')"
       >
         Uplink
         <span class="font-medium text-xs sm:text-lg">(Contacto)</span>
       </button>
       <button
-        class="text-green-500 md:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn animate__delay-1s flex flex-col items-center justify-center"
+        class="text-green-500 lg:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn animate__delay-1s flex flex-col items-center justify-center"
         @click="MapComponentRef?.goToPoint('Operations')"
       >
         Operations
         <span class="font-medium text-xs sm:text-lg">(Proyectos)</span>
       </button>
       <button
-        class="text-green-500 md:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn flex flex-col items-center justify-center"
+        class="text-green-500 lg:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn flex flex-col items-center justify-center"
         @click="MapComponentRef?.goToPoint('Systems')"
       >
         Systems
         <span class="font-medium text-xs sm:text-lg">(Habilidades)</span>
       </button>
       <button
-        class="text-green-500 md:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn animate__delay-1s flex flex-col items-center justify-center"
+        class="text-green-500 lg:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn animate__delay-1s flex flex-col items-center justify-center"
         @click="MapComponentRef?.goToPoint('Intel')"
       >
         Intel
@@ -37,7 +37,7 @@
       </button>
     </div>
     <!-- Map -->
-    <div class="w-full md:w-2/4 p-10 h-full flex items-center justify-center">
+    <div class="w-full lg:w-2/4 p-5 lg:p-10 h-full flex items-center justify-center">
       <div
         ref="map"
         class="border-2 h-full w-full border-green-500 transition-all duration-500 overflow-hidden"
@@ -48,31 +48,31 @@
     </div>
     <!-- Buttons -->
     <div
-      class="items-center md:gap-y-10 w-full md:w-1/4 md:h-full md:pl-0 px-10 pb-10 md:py-10 justify-center md:justify-normal grid grid-cols-2 md:grid-cols-1 gap-4"
+      class="items-center lg:gap-y-10 w-full lg:w-1/4 lg:h-full lg:pl-0 px-5 lg:px-10 pb-10 lg:py-10 justify-center lg:justify-normal grid grid-cols-2 lg:grid-cols-1 gap-4"
     >
       <button
-        class="text-green-500 md:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn animate__delay-2s flex flex-col items-center justify-center"
+        class="text-green-500 lg:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn animate__delay-2s flex flex-col items-center justify-center"
         @click="MapComponentRef?.goToPoint('Uplink')"
       >
         Uplink
         <span class="font-medium text-xs sm:text-lg">(Contacto)</span>
       </button>
       <button
-        class="text-green-500 md:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn animate__delay-1s flex flex-col items-center justify-center"
+        class="text-green-500 lg:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn animate__delay-1s flex flex-col items-center justify-center"
         @click="MapComponentRef?.goToPoint('Operations')"
       >
         Operations
         <span class="font-medium text-xs sm:text-lg">(Proyectos)</span>
       </button>
       <button
-        class="text-green-500 md:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn flex flex-col items-center justify-center"
+        class="text-green-500 lg:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn flex flex-col items-center justify-center"
         @click="MapComponentRef?.goToPoint('Systems')"
       >
         Systems
         <span class="font-medium text-xs sm:text-lg">(Habilidades)</span>
       </button>
       <button
-        class="text-green-500 md:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn animate__delay-2s flex flex-col items-center justify-center"
+        class="text-green-500 lg:text-4xl border-2 p-2 border-green-500 hover:bg-green-500 hover:text-black font-bold hover:cursor-pointer w-full h-full animate__animated animate__fadeIn animate__delay-2s flex flex-col items-center justify-center"
         @click="MapComponentRef?.goToPoint('Alerts')"
       >
         Alerts
