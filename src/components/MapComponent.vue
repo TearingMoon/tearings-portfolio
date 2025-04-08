0abb146042b2fb672954d929d71d5745d8303d8d
David Torrubia <davidtorrubiasantos@gmail.com>
2025-04-08 21:46:47 +0200

    Update MapComponent to utilize MapRenderer for improved rendering logic; refactor SVG handling and remove unused code

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,32 +1,17 @@
 <template>
   <div class="w-full h-full relative">
-    <svg class="w-full h-full text-green-500" ref="MainSvg"></svg>
+    <svg class="w-full h-full text-green-500" ref="mainSvg"></svg>
     <div class="flex items-center justify-center w-full h-full"></div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted, onBeforeUnmount } from 'vue'
-import * as d3 from 'd3'
-import * as topojson from 'topojson-client'
-import type { Topology } from 'topojson-specification'
+import MapRenderer from '@/typescript/MapRenderer'
 
-const MainSvg = ref<SVGSVGElement | null>(null)
+const mainSvg = ref<SVGSVGElement | null>(null)
 
-let rotation: [number, number] = [0, -10] // Rotación inicial
-
-const offset = 10
-
-let land: any = null
-let svg: any = null
-let svgSize = { width: 0, height: 0 }
-let ortoProjection: d3.GeoProjection | null = null
-let path: d3.GeoPath | null = null
-let radius = 0
-let radiusModifier = 1
-const graticule = d3.geoGraticule()
-
-const observer = new ResizeObserver(handleResize)
+const mapRenderer = ref<MapRenderer | null>(null)
 
 const dataPoints: { name: string; longitude: number; latitude: number; url: string }[] = [
   {
@@ -68,251 +53,20 @@ defineExpose({
 })
 
 onMounted(() => {
-  if (!MainSvg.value) return
-  observer.observe(MainSvg.value)
-  svg = d3.select(MainSvg.value)
-  handleResize()
-  setupDrag()
-  loadWorldMap()
-  draw()
+  if (mainSvg.value != null) {
+    mapRenderer.value = new MapRenderer({
+      svg: mainSvg.value,
+      displayablePoints: dataPoints
+    })
+  }
 })
 
 onBeforeUnmount(() => {
-  observer.disconnect()
+  mapRenderer.value?.End()
 })
 
-function handleResize() {
-  svgSize.width = MainSvg.value?.clientWidth || 500
-  svgSize.height = MainSvg.value?.clientHeight || 500
-  radius = Math.max(Math.min(svgSize.width, svgSize.height) / 2 - offset, offset)
-  requestAnimationFrame(draw)
-}
-
-let isDrawing = false
-function draw() {
-  if (!MainSvg.value || isDrawing) return
-  isDrawing = true
-  ortoProjection = d3
-    .geoOrthographic()
-    .scale(radius * radiusModifier)
-    .translate([svgSize.width / 2, svgSize.height / 2])
-    .clipAngle(90)
-    .rotate(rotation)
-
-  path = d3.geoPath().projection(ortoProjection).pointRadius(2)
-
-  clearSvg()
-  svg
-    .attr('preserveAspectRatio', 'xMidYMid meet')
-    .attr('viewBox', `0 0 ${svgSize.width} ${svgSize.height}`)
-
-  drawGraticule(path)
-  drawWorldMap(path)
-  drawPoints(path, ortoProjection)
-
-  isDrawing = false
-}
-
-let canDrag = true
-function setupDrag() {
-  if (!MainSvg.value) return
-
-  const drag = d3
-    .drag<SVGSVGElement, unknown>()
-    .on('drag', (event) => {
-      if (!canDrag) return
-      rotation[0] = (rotation[0] + event.dx * 0.5) % 360
-      rotation[1] = (rotation[1] - event.dy * 0.5) % 360
-      draw()
-    })
-    .on('end', () => {
-      if (radiusModifier !== 1) {
-        d3.transition()
-          .duration(2000)
-          .tween('radius', () => {
-            const r = d3.interpolate(radiusModifier, 1)
-            return (t) => {
-              radiusModifier = r(t)
-              draw()
-            }
-          })
-      }
-    })
-
-  d3.select(MainSvg.value).call(drag)
-}
-
-let landPromise: Promise<any> | null = null
-function loadWorldMap() {
-  if (!landPromise) {
-    landPromise = d3
-      .json('/json/land-110m.json')
-      .then((world) => {
-        if (!world) throw new Error('No se pudo cargar el mapa mundial')
-        land = topojson.feature(world as Topology, (world as Topology).objects.land)
-        draw()
-      })
-      .catch(console.error)
-  }
-}
-
-function clearSvg() {
-  svg.selectAll('*').remove()
-}
-
-function drawWorldMap(path: d3.GeoPath) {
-  if (land != null) {
-    svg
-      .append('path')
-      .datum(land)
-      .attr('d', path)
-      .style('fill', 'rgba(0, 255, 0, 0.05)')
-      .attr('stroke', 'oklch(0.723 0.219 149.579)')
-  }
-}
-
-function drawGraticule(path: d3.GeoPath) {
-  svg
-    .append('circle')
-    .attr('cx', svgSize.width / 2)
-    .attr('cy', svgSize.height / 2)
-    .attr('r', radius * radiusModifier)
-    .attr('fill', 'none')
-    .attr('stroke', 'oklch(0.723 0.219 149.579)')
-    .attr('stroke-width', 1)
-
-  svg
-    .append('path')
-    .datum(graticule())
-    .attr('d', path)
-    .attr('fill', 'none')
-    .attr('stroke', 'oklch(0.723 0.219 149.579)')
-    .attr('stroke-width', 0.5)
-    .attr('opacity', 0.7)
-}
-
-function drawPoints(path: d3.GeoPath, ortoProjection: d3.GeoProjection) {
-  svg
-    .selectAll('.point')
-    .data(dataPoints)
-    .enter()
-    .append('circle')
-    .attr('class', 'point')
-    .attr('cx', (d: any) => {
-      const projected = ortoProjection([d.longitude, d.latitude])
-      return projected ? projected[0] : -100
-    })
-    .attr('cy', (d: any) => {
-      const projected = ortoProjection([d.longitude, d.latitude])
-      return projected ? projected[1] : -100
-    })
-    .attr('r', 5)
-    .attr('fill', 'oklch(0.723 0.219 149.579)')
-    .attr('stroke', 'oklch(0.723 0.219 149.579)')
-    .attr('stroke-width', 1)
-    .attr('visibility', (d: any) => getVisibility(d, ortoProjection))
-    .style('cursor', 'pointer')
-    .on('click', (event: any, d: any) => goToPoint(d.name))
-  svg
-    .selectAll('.point-label')
-    .data(dataPoints)
-    .enter()
-    .append('text')
-    .attr('class', 'point-label')
-    .each(function (this: SVGTextElement, d: any) {
-      const projected = ortoProjection([d.longitude, d.latitude])
-      d3.select(this)
-        .attr('x', projected ? projected[0] : -100)
-        .attr('y', projected ? projected[1] : -100)
-    })
-    .attr('dy', -10)
-    .attr('text-anchor', 'middle')
-    .attr('fill', 'oklch(0.723 0.219 149.579)')
-    .attr('font-size', 30)
-    .attr('font-weight', 'bold')
-    .style('cursor', 'pointer')
-    .text((d: any) => d.name)
-    .attr('visibility', (d: any) => getVisibility(d, ortoProjection))
-    .on('click', (event: any, d: any) => goToPoint(d.name))
-}
-
-function getVisibility(d: any, projection: d3.GeoProjection) {
-  let visible = false
-  const stream = projection.stream({
-    point() {
-      visible = true
-    },
-    lineEnd: function (): void {
-      throw new Error('Function not implemented.')
-    },
-    lineStart: function (): void {
-      throw new Error('Function not implemented.')
-    },
-    polygonEnd: function (): void {
-      throw new Error('Function not implemented.')
-    },
-    polygonStart: function (): void {
-      throw new Error('Function not implemented.')
-    }
-  })
-  stream.point(d.longitude, d.latitude)
-
-  return visible ? 'visible' : 'hidden'
-}
-
 async function goToPoint(name: string) {
-  const point = dataPoints.find((p) => p.name === name)
-  if (!point) return
-  const invertedRotation: [number, number] = [360 - rotation[0], 360 - rotation[1]]
-  transitionToPoint(point, invertedRotation)
-}
-
-function transitionToPoint(
-  point: {
-    latitude: number
-    longitude: number
-  },
-  invertedRotation: [number, number]
-) {
-  if (radiusModifier !== 1) {
-    d3.transition()
-      .duration(2000)
-      .tween('radius', () => {
-        const r = d3.interpolate(radiusModifier, 1)
-        return (t) => {
-          radiusModifier = r(t)
-          draw()
-        }
-      })
-      .on('end', () =>
-        d3
-          .transition()
-          .duration(2000)
-          .tween('rotate', () => {
-            const r1 = d3.interpolate(rotation, [-point.longitude, -point.latitude])
-            const r2 = d3.interpolate(invertedRotation, [-point.longitude, -point.latitude])
-
-            return (t) => {
-              rotation = [Math.min(r1(t)[0], r2(t)[0]), Math.min(r1(t)[1], r2(t)[1])]
-              radiusModifier = 1 + t * 0.5
-              draw()
-            }
-          })
-      )
-  } else {
-    d3.transition()
-      .duration(2000)
-      .tween('rotate', () => {
-        const r1 = d3.interpolate(rotation, [-point.longitude, -point.latitude])
-        const r2 = d3.interpolate(invertedRotation, [-point.longitude, -point.latitude])
-
-        return (t) => {
-          rotation = [Math.min(r1(t)[0], r2(t)[0]), Math.min(r1(t)[1], r2(t)[1])]
-          radiusModifier = 1 + t * 0.5
-          draw()
-        }
-      })
-  }
+  mapRenderer.value?.GoToDisplayablePoint(name)
 }
 </script>
 
---
+++ src/typescript/MapRenderer.ts
@@ -0,0 +1,301 @@
+import * as d3 from 'd3'
+import * as topojson from 'topojson-client'
+import type { Topology } from 'topojson-specification'
+
+interface MapRedererParams {
+  svg: SVGSVGElement
+  offset?: number
+  rotation?: [number, number]
+  displayablePoints?: DisplayablePoint[]
+  worldMapLocation?: string
+}
+
+/**
+ * A class representing a map renderer.
+ * @class MapRenderer
+ */
+export default class MapRenderer {
+  sizeObserver: ResizeObserver = new ResizeObserver(this.HandleResize.bind(this))
+
+  displayablePoints: DisplayablePoint[]
+
+  svg: SVGSVGElement
+  svgSelection: d3.Selection<SVGSVGElement, unknown, null, undefined>
+  svgSize: [number, number] = [0, 0]
+
+  sphereRadius: number
+  zoom: number = 1
+  get endRadius(): number {
+    if (!this.sphereRadius) return 0
+    if (!this.svgSize) return 0
+    return this.sphereRadius * this.zoom
+  }
+
+  offset: number
+  rotation: [number, number]
+
+  worldMapLocation: string
+  worldMap:
+    | GeoJSON.Feature<GeoJSON.GeometryObject, GeoJSON.GeoJsonProperties>
+    | GeoJSON.FeatureCollection<GeoJSON.GeometryObject, GeoJSON.GeoJsonProperties>
+    | null = null
+  requestAnimationFrameId: number | null = null
+
+  canInteract: boolean = true
+
+  constructor({
+    svg,
+    offset = 10,
+    rotation = [0, -10],
+    displayablePoints = [],
+    worldMapLocation = '/json/land-110m.json'
+  }: MapRedererParams) {
+    this.svg = svg
+    this.svgSelection = d3.select(svg)
+
+    this.offset = offset
+    this.rotation = rotation
+    this.svgSize[0] = svg.clientWidth // Get the initial width of the SVG element
+    this.svgSize[1] = svg.clientHeight // Get the initial height of the SVG element
+    this.sphereRadius = this.CalculateRadius() // Calculate the radius of the initial sphere based on the SVG size and offset
+    this.displayablePoints = displayablePoints
+    this.worldMapLocation = worldMapLocation
+
+    this.Start()
+  }
+
+  Start() {
+    this.sizeObserver.observe(this.svg as Element)
+    this.LoadWorldMap()
+    this.SetUpControls()
+  }
+
+  End() {
+    if (this.requestAnimationFrameId) {
+      cancelAnimationFrame(this.requestAnimationFrameId)
+    }
+    this.sizeObserver.disconnect()
+  }
+
+  HandleResize() {
+    console.log('Resizing...')
+    if (this.svg) {
+      const rect = this.svg.getBoundingClientRect()
+      this.svgSize[0] = rect.width
+      this.svgSize[1] = rect.height
+      this.sphereRadius = this.CalculateRadius()
+    }
+    this.DrawFrame()
+  }
+
+  CalculateRadius() {
+    return Math.max(Math.min(this.svgSize[0], this.svgSize[1]) / 2 - this.offset, this.offset)
+  }
+
+  SetUpControls() {
+    this.SetUpDragControls()
+    this.SetUpScrollControls()
+  }
+
+  SetUpDragControls() {
+    const drag = d3.drag<SVGSVGElement, unknown>().on('drag', (event) => {
+      if (!this.canInteract) return
+      this.rotation[0] = (this.rotation[0] + event.dx * 0.5) % 360
+      this.rotation[1] = (this.rotation[1] - event.dy * 0.5) % 360
+      this.DrawFrame()
+    })
+    this.svgSelection.call(drag)
+  }
+
+  SetUpScrollControls() {
+    const zoom = d3
+      .zoom<SVGSVGElement, unknown>()
+      .scaleExtent([1, 2])
+      .on('zoom', (event) => {
+        if (!this.canInteract) return
+        this.zoom = event.transform.k
+        this.DrawFrame()
+      })
+    this.svgSelection.call(zoom)
+  }
+
+  LoadWorldMap() {
+    d3.json(this.worldMapLocation)
+      .then((worldMap) => {
+        this.worldMap = topojson.feature(worldMap as Topology, (worldMap as Topology).objects.land)
+      })
+      .catch((error) => {
+        console.error('Error loading world map:', error)
+      })
+  }
+
+  DrawFrame() {
+    this.requestAnimationFrameId = requestAnimationFrame(this.Draw.bind(this))
+  }
+
+  isDrawing: boolean = false
+  Draw() {
+    if (this.isDrawing) return
+    this.isDrawing = true
+
+    const ortoProjection = d3
+      .geoOrthographic()
+      .scale(this.endRadius)
+      .translate([this.svgSize[0] / 2, this.svgSize[1] / 2])
+      .clipAngle(90)
+      .rotate(this.rotation)
+    const path = d3.geoPath().projection(ortoProjection).pointRadius(2)
+    this.svgSelection.selectAll('*').remove() // Clear the SVG before drawing
+    this.svgSelection
+      .attr('preserveAspectRatio', 'xMidYMid meet')
+      .attr('viewBox', `0 0 ${this.svgSize[0]} ${this.svgSize[1]}`)
+
+    this.DrawGraticule(path)
+    this.DrawWorldMap(path)
+    this.DrawDisplayablePoints(ortoProjection)
+
+    this.isDrawing = false
+  }
+
+  DrawGraticule(path: d3.GeoPath) {
+    this.svgSelection
+      .append('circle')
+      .attr('cx', this.svgSize[0] / 2)
+      .attr('cy', this.svgSize[1] / 2)
+      .attr('r', this.endRadius)
+      .attr('fill', 'none')
+      .attr('stroke', 'oklch(0.723 0.219 149.579)')
+      .attr('stroke-width', 1)
+
+    this.svgSelection
+      .append('path')
+      .datum(d3.geoGraticule())
+      .attr('d', path)
+      .attr('fill', 'none')
+      .attr('stroke', 'oklch(0.723 0.219 149.579)')
+      .attr('stroke-width', 0.5)
+      .attr('opacity', 0.7)
+  }
+
+  DrawWorldMap(path: d3.GeoPath) {
+    if (!this.worldMap) return
+    this.svgSelection
+      .append('path')
+      .datum(this.worldMap)
+      .attr('d', path)
+      .style('fill', 'rgba(0, 255, 0, 0.05)')
+      .attr('stroke', 'oklch(0.723 0.219 149.579)')
+  }
+
+  DrawDisplayablePoints(ortoProjection: d3.GeoProjection) {
+    this.svgSelection
+      .selectAll('.point')
+      .data(this.displayablePoints)
+      .enter()
+      .append('circle')
+      .attr('class', 'point')
+      .attr('cx', (d: any) => {
+        const projected = ortoProjection([d.longitude, d.latitude])
+        return projected ? projected[0] : -100
+      })
+      .attr('cy', (d: any) => {
+        const projected = ortoProjection([d.longitude, d.latitude])
+        return projected ? projected[1] : -100
+      })
+      .attr('r', 5)
+      .attr('fill', 'oklch(0.723 0.219 149.579)')
+      .attr('stroke', 'oklch(0.723 0.219 149.579)')
+      .attr('stroke-width', 1)
+      .attr('visibility', (d: Coordinate) => this.GetElementVisibility(d, ortoProjection))
+      .style('cursor', 'pointer')
+      .on('click', (event: any, d: DisplayablePoint) => this.GoToDisplayablePoint(d.name))
+
+    this.svgSelection
+      .selectAll('.point-label')
+      .data(this.displayablePoints)
+      .enter()
+      .append('text')
+      .attr('class', 'point-label')
+      .each(function (this: SVGTextElement, d: any) {
+        const projected = ortoProjection([d.longitude, d.latitude])
+        d3.select(this)
+          .attr('x', projected ? projected[0] : -100)
+          .attr('y', projected ? projected[1] : -100)
+      })
+      .attr('dy', -10)
+      .attr('text-anchor', 'middle')
+      .attr('fill', 'oklch(0.723 0.219 149.579)')
+      .attr('font-size', 30)
+      .attr('font-weight', 'bold')
+      .style('cursor', 'pointer')
+      .text((d: any) => d.name)
+      .attr('visibility', (d: Coordinate) => this.GetElementVisibility(d, ortoProjection))
+      .on('click', (event: any, d: DisplayablePoint) => this.GoToDisplayablePoint(d.name))
+  }
+
+  GetElementVisibility(coordinate: Coordinate, projection: d3.GeoProjection): 'visible' | 'hidden' {
+    let isVisible = false
+    const stream = projection.stream({
+      point() {
+        isVisible = true
+      },
+      lineEnd: function (): void {
+        throw new Error('Function not implemented.')
+      },
+      lineStart: function (): void {
+        throw new Error('Function not implemented.')
+      },
+      polygonEnd: function (): void {
+        throw new Error('Function not implemented.')
+      },
+      polygonStart: function (): void {
+        throw new Error('Function not implemented.')
+      }
+    })
+    stream.point(coordinate.longitude, coordinate.latitude)
+    return isVisible ? 'visible' : 'hidden'
+  }
+
+  GoToDisplayablePoint(name: string) {
+    console.log('Going to:', name)
+    const point = this.displayablePoints.find((p) => p.name === name)
+    if (!point) return
+    this.TransitionToCoord(point)
+  }
+
+  TransitionToCoord(coord: Coordinate) {
+    this.canInteract = false
+    const invertedRotation: [number, number] = [360 - this.rotation[0], 360 - this.rotation[1]]
+    console.log('Desired Coord:', coord)
+    console.log('Inverted Rotation:', invertedRotation)
+
+    this.canInteract = true
+  }
+}
+
+/**
+ * A class representing a point on the map.
+ * @class DisplayablePoints
+ */
+export class DisplayablePoint {
+  name: string
+  longitude: number
+  latitude: number
+  url: string
+
+  constructor(name: string, longitude: number, latitude: number, url: string) {
+    this.name = name
+    this.longitude = longitude
+    this.latitude = latitude
+    this.url = url
+  }
+}
+
+class Coordinate {
+  longitude: number
+  latitude: number
+  constructor(longitude: number, latitude: number) {
+    this.longitude = longitude
+    this.latitude = latitude
+  }
+}
