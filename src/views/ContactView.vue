b01b875da57f9826898e6406c9edccdba02ae301
Tearing <davidtorrubiasantos@gmail.com>
2026-09-03 18:05:46 +0200

    feat: update LinkedIn contact link and remove unused icon import

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import ContactLink from "@/components/contact/Contactlink.vue";
 import { Mail } from "lucide-vue-next";
-import { siGithub, siLinkerd } from "simple-icons";
+import { siGithub } from "simple-icons";
 </script>
 
 <template>
@@ -39,12 +39,18 @@ import { siGithub, siLinkerd } from "simple-icons";
       </ContactLink>
       <ContactLink
         label="LinkedIn"
-        value="TearingMoon"
+        value="David Torrubia"
         href="https://www.linkedin.com/in/david-torrubia-santos-341628236/"
       >
         <template #icon>
-          <svg viewBox="0 0 24 24" class="size-5 fill-current">
-            <path :d="siLinkerd.path" />
+          <svg
+            viewBox="0 0 24 24"
+            class="size-5 fill-current"
+            aria-hidden="true"
+          >
+            <path
+              d="M5.17 3.5A2.17 2.17 0 1 1 .83 3.5a2.17 2.17 0 0 1 4.34 0ZM1.2 7h3.94v12H1.2V7Zm6.39 0h3.78v1.64h.05c.53-1 1.81-2.06 3.73-2.06 3.99 0 4.73 2.63 4.73 6.05V19h-3.94v-5.64c0-1.35-.02-3.08-1.88-3.08-1.88 0-2.17 1.47-2.17 2.98V19H7.59V7Z"
+            />
           </svg>
         </template>
       </ContactLink>
